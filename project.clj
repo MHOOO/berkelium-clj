82e5fd0339ca33f2e14b39c9e3e4600200f36727
Thomas Karolski <Thomas.Karolski@googlemail.com>
2012-12-07 19:41:54 +0100

    - update project file

--- project.clj
+++ project.clj
@@ -6,7 +6,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure.contrib/core "1.3.0-alpha4"]
                  [org.clojure.contrib/logging "1.3.0-alpha4"]]
-  ;; :source-paths ["src/clj"]
-  ;; :java-source-paths ["src/java"]
   :main de.karolski.berkelium-clj.core
 )

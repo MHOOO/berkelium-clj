36c9360b07fec1ba6b1e53cf305041d20b851dee
Thomas Karolski <thomas.karolski@googlemail.com>
2012-12-14 12:55:06 +0100

    - fix project artifact

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.karolski.berkelium-clj "0.1.0-SNAPSHOT"
+(defproject de.karolski/berkelium-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
